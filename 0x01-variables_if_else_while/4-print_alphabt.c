0229210f4c72c4e73bfb2bb520d8979326e8895c
bashar badawi <basharbadawi32@gmail.com>
2023-02-23 03:53:50 -0800

    alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+    char c;
+    for(c='a'; c<='z'; c++)
+    {
+        if(c=='q'||c=='e')
+        {
+            continue;
+        }
+        putchar(c);
+        putchar('\n');
+    }
+}
