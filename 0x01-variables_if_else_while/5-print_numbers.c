9a66f0c5710cc63799169c0c06104efac0df6b1b
bashar badawi <basharbadawi32@gmail.com>
2023-02-23 04:37:41 -0800

    prinitng

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,17 @@
+
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 (Sucess)
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    for(int i=0; i<10; i++)
-    {
-        printf("%d",i);
-    }
-    printf('\n');
-    return(0);
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
 }
